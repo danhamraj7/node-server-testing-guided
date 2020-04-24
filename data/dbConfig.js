b4ab4cc14007676804f25dd99b9566b753e57975
danhamraj <danhamraj@gmail.com>
2020-04-24 07:12:34 -0400

    updated dbcondfit

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,6 +1,12 @@
+// const knex = require("knex");
+// const config = require("../knexfile.js");
+
+// const environment = process.env.DB_ENV || "development";
+
+// module.exports = knex(config[environment]);
+
 const knex = require("knex");
-const config = require("../knexfile.js");
 
-const environment = process.env.DB_ENV || "development";
+const configOptions = require("../knexfile").development;
 
-module.exports = knex(config[environment]);
+module.exports = knex(configOptions);
